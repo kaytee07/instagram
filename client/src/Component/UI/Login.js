0540cfa74a01906710a1f520e7877f785e717ac1
Kaytee07 <kofi.taylor07@gmail.com>
2022-01-07 18:34:02 +0000

    Signin and signup linked to backend

--- client/src/Component/UI/Login.js
+++ client/src/Component/UI/Login.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from 'react-router-dom'
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+ const postData = () => {
+    fetch("http://localhost:2000/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: new URLSearchParams({
+        password,
+        email
+      }),
+    });
+  }
+
  return (
    <div className="d-flex justify-content-center mt-5">
      <div className="card" style={{ width: "22rem" }}>
        <div className="card-body">
-         <h5 className="card-title signin_title" style={{textAlign:"center"}}>The gram</h5>
+         <h5
+           className="card-title signin_title"
+           style={{ textAlign: "center" }}
+         >
+           The gram
+         </h5>
          <form>
            <div className="mb-3">
-             <label for="exampleInputEmail1" className="form-label">
+             <label htmlFor="exampleInputEmail1" className="form-label">
                Email address
              </label>
              <input
                type="email"
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
                className="form-control"
                id="exampleInputEmail1"
                aria-describedby="emailHelp"
@@ -23,17 +46,19 @@ function Login() {
              </div>
            </div>
            <div className="mb-3">
-             <label for="exampleInputPassword1" className="form-label">
+             <label htmlFor="exampleInputPassword1" className="form-label">
                Password
              </label>
              <input
                type="password"
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
                className="form-control"
                id="exampleInputPassword1"
              />
            </div>
-        
-           <button type="submit" className="btn btn-primary">
+
+           <button type="submit" onClick={()=> postData()} className="btn btn-primary">
              Sign in
            </button>
            <div>
--- client/src/Component/UI/Signup.js
+++ client/src/Component/UI/Signup.js
@@ -1,29 +1,53 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const postData = () => {
+    fetch("http://localhost:2000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: new URLSearchParams({
+        name:username,
+        password,
+        email
+      }),
+    });
+  }
+
   return (
     <div className="d-flex justify-content-center mt-5">
-      <div className="card" style={{ maxWidth:"28rem"}}>
+      <div className="card" style={{ maxWidth: "28rem" }}>
         <div className="card-body">
           <h5 className="card-title signin_title">The gram</h5>
           <form>
             <div className="mb-3">
               <div className="mb-3">
-                <label for="exampleInputUsername1" className="form-label">
+                <label htmlFor="exampleInputUsername1" className="form-label">
                   username
                 </label>
                 <input
                   type="text"
+                  placeholder="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                   className="form-control"
                   id="exampleInputUsername1"
                 />
               </div>
-              <label for="exampleInputEmail1" className="form-label">
+              <label htmlFor="exampleInputEmail1" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
@@ -33,17 +57,20 @@ function Signup() {
               </div>
             </div>
             <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">
+              <label htmlFor="exampleInputPassword1" className="form-label">
                 Password
               </label>
               <input
                 type="password"
+                placeholder="email"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="form-control"
                 id="exampleInputPassword1"
               />
             </div>
 
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" onClick={()=> postData()} className="btn btn-primary">
               Sign up
             </button>
             <div>
